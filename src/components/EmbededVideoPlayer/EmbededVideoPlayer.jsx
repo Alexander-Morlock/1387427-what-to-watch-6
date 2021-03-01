5c1ebd2f234344aba09d848e387b9164b6d7134c
Morlock <onlinemania@email.cz>
2021-03-01 16:35:36 +0300

    module4-task1a

--- src/components/EmbededVideoPlayer/EmbededVideoPlayer.jsx
+++ src/components/EmbededVideoPlayer/EmbededVideoPlayer.jsx
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 // import {Link} from 'react-router-dom';
 
 const EmbededVideoPlayer = (props) => {
-  const [isMuted, toggleIsMuted] = useState(true);
   return (
-    <video {...props} src={props.src} poster={props.poster} muted={isMuted} autoPlay onMouseOut={props.onMouseOut}>
+    <video {...props}
+      src={props.src}
+      poster={props.poster}
+      muted autoPlay
+      onMouseOut={props.onMouseOut}>
     </video>
   );
-
 };
 
 EmbededVideoPlayer.propTypes = {

f6baa1ed0c3ac401a601c06ac4002f7b3d1f3d65
Morlock <onlinemania@email.cz>
2021-03-01 17:51:43 +0300

    module4-task1d

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -29,16 +29,14 @@ const MovieCard = (props) => {
         width="280" height="175"
         onMouseOutCallback={onMouseOutHandler} />
     </article>
-    : (
-      <article className="small-movie-card catalog__movies-card">
-        <div className="small-movie-card__image" data-id={props.id} onMouseOver={onMouseOverHandler}>
-          <img src={props.preview_image} alt={props.name} width="280" height="175" />
-        </div>
-        <h3 className="small-movie-card__title">
-          <Link className="small-movie-card__link" to={`/films/${props.id}`}>{props.name}</Link>
-        </h3>
-      </article>
-    );
+    : <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image" data-id={props.id} onMouseOver={onMouseOverHandler}>
+        <img src={props.preview_image} alt={props.name} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <Link className="small-movie-card__link" to={`/films/${props.id}`}>{props.name}</Link>
+      </h3>
+    </article>;
 };
 
 MovieCard.propTypes = {

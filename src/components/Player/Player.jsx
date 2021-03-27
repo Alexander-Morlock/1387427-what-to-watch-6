6f0e89f3d48a59b03f57b9acc62fda4c505ac412
Morlock <onlinemania@email.cz>
2021-03-28 00:37:15 +0100

    6-1d

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React, {useRef, useState} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import shapeOfMovie from '../../utils/shape-of-movie';
 import {connect} from 'react-redux';
@@ -11,27 +11,65 @@ const Player = (props) => {
   const movie = props.movies.find((m) => m.id === +id);
   const movieTitle = movie.name;
 
+  const history = useHistory();
+  const closePlayer = () => history.push(`/films/${id}`);
+
+  const videoRef = useRef();
+  let togglerPosition = 0;
+  if (videoRef.current) {
+    togglerPosition = videoRef.current.currentTime / videoRef.current.duration * 100;
+  }
+
+  const getDuration = () => `${Math.floor((movie.run_time) / 60)}:${Math.floor(movie.run_time % 60)}:00`;
+  const [buttonState, setButtonState] = useState({isStarted: false, isPlayback: false});
+
+  const playButtonHandler = () => {
+    setButtonState((prevState) => ({isStarted: true, isPlayback: !prevState.isPlayback}));
+  };
+
+  if (videoRef.current) {
+    if (buttonState.isPlayback) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }
+
+  const toggleFullScreen = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  };
+
   return (
     <div className="player">
-      <video src="#" className="player__video" poster="img/player-poster.jpg" />
-      <button type="button" className="player__exit">Exit</button>
+      <video ref={videoRef} src={movie.video_link} className="player__video" poster="img/player-poster.jpg"/>
+      <button type="button" className="player__exit" onClick={closePlayer}>Exit</button>
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            <progress className="player__progress" value={30} max={100} />
-            <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
+            <progress className="player__progress" value={togglerPosition} max={100} />
+            <div className="player__toggler" style={{left: `${togglerPosition}%`}}>Toggler</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
+          <div className="player__time-value">{getDuration()}</div>
         </div>
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width={19} height={19}>
-              <use xlinkHref="#play-s" />
-            </svg>
+          <button type="button" className="player__play" onClick={playButtonHandler}>
+            {
+              !buttonState.isPlayback || !buttonState.isStarted
+                ? <svg viewBox="0 0 19 19" width={19} height={19}>
+                  <use xlinkHref="#play-s" />
+                </svg>
+                : <svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"></use>
+                </svg>
+            }
             <span>Play</span>
           </button>
           <div className="player__name">{movieTitle}</div>
-          <button type="button" className="player__full-screen">
+          <button type="button" className="player__full-screen" onClick={toggleFullScreen}>
             <svg viewBox="0 0 27 27" width={27} height={27}>
               <use xlinkHref="#full-screen" />
             </svg>
